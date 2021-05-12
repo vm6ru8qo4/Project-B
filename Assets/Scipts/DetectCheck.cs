d93c2addb88f43cfa68c681d7e4886d14120e33c
vm6ru8qo4 <b97501033@gmail.com>
2021-05-12 20:22:23 +0800

    add ui and detect

---
+++ Assets/Scipts/DetectCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCheck : MonoBehaviour
+{
+    SpriteRenderer m_TargetRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_TargetRenderer = this.GetComponent<SpriteRenderer>();
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision){
+        m_TargetRenderer.color = Color.red;
+        Debug.Log("detected!");
+    }
+    void OnCollisionExit2D(Collision2D collision){
+        m_TargetRenderer.color = Color.white;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scipts/GameUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+
+    [SerializeField] Text m_MessageText;
+
+    public void ShowMessage (string msg){m_MessageText.text = "Me: " + msg; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
